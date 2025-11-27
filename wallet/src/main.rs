aee6f45a8b973027d9875fa3a3bcfad1c80c031c
Daniil Polyakov <arjentix@gmail.com>
2025-11-27 22:07:53 +0300

    refactor: move some stuff to a more suitable place

--- wallet/src/main.rs
+++ wallet/src/main.rs
@@ -1,14 +1,16 @@
 use anyhow::Result;
-use clap::{CommandFactory, Parser};
+use clap::{CommandFactory as _, Parser as _};
 use tokio::runtime::Builder;
-use wallet::{Args, OverCommand, execute_continious_run, execute_setup, execute_subcommand};
+use wallet::cli::{Args, OverCommand, execute_continious_run, execute_setup, execute_subcommand};
 
 pub const NUM_THREADS: usize = 2;
 
 // TODO #169: We have sample configs for sequencer, but not for wallet
 // TODO #168: Why it requires config as a directory? Maybe better to deduce directory from config
-// file path? TODO #172: Why it requires config as env var while sequencer_runner accepts as
-// argument? TODO #171: Running pinata doesn't give output about transaction hash and etc.
+// file path?
+// TODO #172: Why it requires config as env var while sequencer_runner accepts as
+// argument?
+// TODO #171: Running pinata doesn't give output about transaction hash and etc.
 fn main() -> Result<()> {
     let runtime = Builder::new_multi_thread()
         .worker_threads(NUM_THREADS)
@@ -24,19 +26,17 @@ fn main() -> Result<()> {
         if let Some(overcommand) = args.command {
             match overcommand {
                 OverCommand::Command(command) => {
-                    execute_subcommand(command).await.unwrap();
+                    let _output = execute_subcommand(command).await?;
+                    Ok(())
                 }
-                OverCommand::Setup { password } => {
-                    execute_setup(password).await.unwrap();
-                }
+                OverCommand::Setup { password } => execute_setup(password).await,
             }
         } else if args.continious_run {
-            execute_continious_run().await.unwrap();
+            execute_continious_run().await
         } else {
             let help = Args::command().render_long_help();
             println!("{help}");
+            Ok(())
         }
-    });
-
-    Ok(())
+    })
 }
